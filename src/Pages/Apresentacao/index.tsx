9d2670580976cae7adc4e323bd6dd473e7ec100f
ALINE PINHELLI <97640506+AlinePinhelli@users.noreply.github.com>
2023-06-28 20:53:28 -0300

    começando o header

--- src/Pages/Apresentacao/index.tsx
+++ src/Pages/Apresentacao/index.tsx
@@ -4,9 +4,15 @@ import { ReactComponent as Logo} from '../../assets/4rent.svg'
 export default function Apresentacao() {
   return(
     <main>
+      
+      <header className={styles.header}>
       <nav className={styles.menu}>
         <Logo /> 
       </nav>
+        <div className={styles.header__text} >
+        Experiência incrível em Curitiba
+        </div>
+      </header>
     </main>
 
     
