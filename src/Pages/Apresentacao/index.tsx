0a336278146b2b9770ad1849a270bcd016f67cc3
ALINE PINHELLI <97640506+AlinePinhelli@users.noreply.github.com>
2023-06-28 15:26:00 -0300

    Inicio do arquivo de apresentação (#1)

---
+++ src/Pages/Apresentacao/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./Apresentacao.module.scss";
+
+export default function Apresentacao() {
+  return (
+    <main>
+      <nav className="menu">
+
+      </nav>
+    </main>
+  )
+}
