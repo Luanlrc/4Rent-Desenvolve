c67534ee3c974b303788d90004e7913bcac76269
ALINE PINHELLI <97640506+AlinePinhelli@users.noreply.github.com>
2023-06-28 21:52:27 -0300

    imagem principal e logo conforme figma

--- src/Pages/Apresentacao/index.tsx
+++ src/Pages/Apresentacao/index.tsx
@@ -1,12 +1,13 @@
 import styles from './Apresentacao.module.scss'
-import { ReactComponent as Logo} from '../../assets/4rent.svg'
+import { ReactComponent as Logo} from '../../assets/Group.svg'
 
 export default function Apresentacao() {
   return(
     <main>
       <header className={styles.header}>
       <nav className={styles.menu}>
-        <Logo /> 
+          <Logo/>
+         <h2 className={styles.menu__text}>  4Rent</h2>
       </nav>
         <div className={styles.header__text} >
         Experiência incrível em Curitiba
