0e5c72e78975f92aaed0e3a8e4fb541300c02e83
Bruu <112636250+brunabettiol@users.noreply.github.com>
2023-07-03 19:25:43 -0300

    Feature/7 (#12)
    * Atualizando carrossel
    
    * Finalização do carrossel de imagens

---
+++ src/Pages/Carrossel/index.tsx
@@ -0,0 +1,29 @@
+import Slider from "react-slick";
+import styles from './Carrossel.module.scss';
+
+function importAllImages(): string[] {
+    const imagesPaths = require.context('/public/assets/carrossel', false, /\.(png|jpe?g|svg)$/);
+    return imagesPaths.keys().map(imagesPaths) as string[];
+}
+export default function Carrosel(){
+    var settings = {
+        dots: true,
+        slidesToShow: 5,
+      };
+
+      const images: string[] = importAllImages();
+      console.log(images)
+      return (
+        <div className={styles.container__carrossel}>
+            <Slider {...settings}>
+                {images.map((image) => {
+                    return (
+                        <div className={styles.card__carrossel} >
+                    <img  className={styles.img__carrossel} src={image}/>
+                </div>
+                    )
+                })}
+            </Slider>
+        </div>
+      );
+    }
